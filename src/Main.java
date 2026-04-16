4d7073af24b36c0f3cbded775670e44c80b0d5a4
shahad <shahadm.albarrak@gmail.com>
2026-04-16 11:48:51 +0300

    "adding setter"

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,9 @@ void main() {
     System.out.printf("%.2f",circle.calculateArea());
     System.out.println("\nthe Circumference of circle of radius:"+circle.getRadius());
     System.out.printf("%.2f",circle.calculateCircumference());
+    Circle circle2=new Circle();
+    circle2.setRadius(4);
+    System.out.printf("\nradius of circle 2 after setting the value:"+circle2.getRadius());
 
     System.out.println("\n\n====Testing Rectangle====");
     Rectangle rectangle=new Rectangle(2,6);
@@ -15,6 +18,11 @@ void main() {
     System.out.println(rectangle.calculateArea());
     System.out.println("the perimeter of height:"+rectangle.getHeight()+" and width:"+rectangle.getWidth());
     System.out.println(rectangle.calculateCircumference());
+    Rectangle rectangle2=new Rectangle();
+    rectangle2.setHeight(3);
+    rectangle2.setWidth(4);
+    System.out.printf("The height and the width of rectangle 2 after setting the value of both\nHeight:"+rectangle2.getHeight()+"\n" +
+            "width:"+rectangle2.getWidth());
 
     System.out.println("\n====Testing Triangle====");
     Triangle triangle=new Triangle(5,5);
@@ -22,6 +30,11 @@ void main() {
     System.out.println(triangle.calculateArea());
     System.out.println("the perimeter of height:"+triangle.getHeight()+" and base:"+triangle.getBase());
     System.out.println(triangle.calculateCircumference());
+    Triangle triangle2=new Triangle();
+    triangle2.setHeight(8);
+    triangle2.setBase(8);
+    System.out.printf("The height and the base of triangle 2 after setting the value of both\nHeight:"+triangle2.getHeight()+"\n" +
+            "base:"+triangle2.getBase());
 
 
 
